c670edc101b2a36dc4249b3183cf391673d86862
fayahmac <bed-com-03-19@unima.ac.mw>
2024-04-21 20:46:15 +0200

    created a header component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import { DataQuery } from '@dhis2/app-runtime'
-import i18n from '@dhis2/d2-i18n'
 import React from 'react'
-import classes from './App.module.css'
+import Header from './components/Header'
 
-const query = {
-    me: {
-        resource: 'me',
-    },
+function App() {
+  return (
+    <div className=''>
+        <Header/>
+    </div>
+  )
 }
 
-const MyApp = () => (
-    <div className={classes.container}>
-        <DataQuery query={query}>
-            {({ error, loading, data }) => {
-                if (error) {
-                    return <span>ERROR</span>
-                }
-                if (loading) {
-                    return <span>...</span>
-                }
-                return (
-                    <>
-                        <h1>
-                            {i18n.t('Hello {{name}}', { name: data.me.name })}
-                        </h1>
-                        <h3>{i18n.t('Welcome to DHIS2!')}</h3>
-                    </>
-                )
-            }}
-        </DataQuery>
-    </div>
-)
-
-export default MyApp
+export default App
