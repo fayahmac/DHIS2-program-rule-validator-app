72ea83216d843432fbdc5b2c7aafe366836259c7
fayahmac <bed-com-03-19@unima.ac.mw>
2024-04-22 10:44:31 +0200

    started creating three cards on the dashboard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import {BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill}from 'react-icons/bs'
 
-function DashboardHome() {
+function Home() {
   return (
     <main className='main-container'>
          <div className='main-title'>
+          <h3>DASHBOARD OVERVIEW</h3>
+          </div>
              <div className='main-cards'>
                  <div className='card'>
-                    <h3>Program Rule Management</h3>
-                    <BsFillArchiveFill className='icon'/>
+                     <BsFillArchiveFill className='icon'/>
+                     <h3>Program Rule Management</h3>
                  </div>
-              </div>
-          </div>    
+                 <div className='card'>
+                     <BsFillArchiveFill className='icon'/>
+                     <h3>Rule Validator</h3>
+                 </div>
+                 <div className='card'>
+                     <BsFillArchiveFill className='icon'/>
+                     <h3>Notifications</h3>
+                 </div>
+              </div> 
     </main>
   )
 }
 
-export default DashboardHome
+export default Home
