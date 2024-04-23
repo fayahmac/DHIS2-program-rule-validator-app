7e79d2838ec06f248ef668576731c6e5438774c9
bed-phy-52-19 <bed-phy-52-19@unima.ac.mw>
2024-04-23 02:13:03 +0200

    side bar updated

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import React from 'react'
 import classes from './App.module.css'
     import './Sidebar.css'; // Import CSS file for styling
 
-
+    import {BsFillArchiveFill, Tooltip, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill}from 'react-icons/bs'
+    import 
+     { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line } 
+     from 'recharts';
     
     
     const MyApp = () => {
       return (
         <div className="sidebar">
           <ul>
-            <h1>dashbord</h1>
+            
+            <h1>DHIS2 PROGRAM VALIDATOR</h1>
             <li>
-            <ul><li><span>dashbord</span></li></ul>
-            <ul><li>dashbord</li></ul>
-            <ul><li>dashbord</li></ul>
-            <ul><li>dashbord</li></ul>
-            <ul><li>dashbord</li></ul>
+            <ul><li><BsFillArchiveFill/><span>PROGRAM RULES</span></li></ul>
+            <ul><BsFillArchiveFill/><li>VALIDATE RULES</li></ul>
+            <ul><li>CONFIGURATION ENGINE</li></ul>
+            <ul><li>NOTIFICATION</li></ul>
+            <ul><BsPeopleFill/><li>USER</li></ul>
+            <ul><li>SETTINGS</li></ul>
             </li>
           </ul>
         </div>
