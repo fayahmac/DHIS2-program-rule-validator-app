ab5ae246bcb4e581be1635e6c00597d61a3d6a2c
bed-phy-52-19 <bed-phy-52-19@unima.ac.mw>
2024-04-24 00:24:38 +0200

    link added to the element of the sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import { DataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
+
 // import classes from './App.module.css'
 import Home from './home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
     const MyApp = () => {
       return (
-       <div>
-        <Home/>
-       </div>
+       <BrowserRouter>
+       <Routes>
+        <Route>
+        <Route path="/" element={<Home />} />
+        </Route>
+              <Route path="/program-rules">
+                {/* Component for Program Rules */}
+              </Route>
+              <Route path="/validate-rules">
+                {/* Component for Validate Rules */}
+              </Route>
+              <Route path="/configuration-engine">
+                {/* Component for Configuration Engine */}
+              </Route>
+              <Route path="/notification">
+                {/* Component for Notification */}
+              </Route>
+              <Route path="/user">
+                {/* Component for User */}
+              </Route>
+              <Route path="/Home">
+                {/* Component for Settings */}
+              </Route>
+        
+    
+        </Routes>
+        
+       </BrowserRouter>
       );
     }
     
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,6 +1,8 @@
 import { DataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
     import './Sidebar.css'; // Import CSS file for styling
     import {BsFillArchiveFill, BsFillGearFill, BsListCheck, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill, BsTools}from 'react-icons/bs'
     import 
@@ -10,22 +12,25 @@ import React from 'react'
   
     const Home = () => {
       return (
-        <div className="sidebar">
-          <ul>
-          
-            <h1>DHIS2 PROGRAM VALIDATOR</h1>
-            <li>
-            <ul><li><BsFillArchiveFill className="valid"/>PROGRAM RULES</li></ul>
-            <ul><li><BsListCheck className="valid"/>VALIDATE RULES</li></ul>
-            <ul><li><BsTools className="valid"/>CONFIGURATION ENGINE</li></ul>
-            <ul><li><BsFillBellFill className="valid"/>NOTIFICATION</li></ul>
-            <ul><li><BsPeopleFill className="valid"/>USER</li></ul>
-            <ul><li><BsFillGearFill className="valid"/>SETTINGS</li></ul>
-            </li>
-          </ul>
-        </div>
-      );
-    }
+            <div className="sidebar">
+              <ul>
+                <h1>DHIS2 PROGRAM VALIDATOR</h1>
+                <li>
+                  
+                <ul><li><Link to="/program-rules" style={{ textDecoration: 'none' }}><BsFillArchiveFill className="valid"/>PROGRAM RULES</Link></li></ul>
+                    <ul><li><Link to="/validate-rules"style={{ textDecoration: 'none' }} ><BsListCheck className="valid"/>VALIDATE RULES</Link></li></ul>
+                    <ul><li><Link to="/configuration-engine" style={{ textDecoration: 'none' }}><BsTools className="valid"/>CONFIGURATION ENGINE</Link></li></ul>
+                   <ul><li><Link to="/notification"style={{ textDecoration: 'none' }} ><BsFillBellFill className="valid"/>NOTIFICATION</Link></li></ul> 
+                   <ul> <li><Link to="/user"style={{ textDecoration: 'none' }} ><BsPeopleFill className="valid"/>USER</Link></li></ul>
+                    <ul><li><Link to="/settings"style={{ textDecoration: 'none' }} ><BsFillGearFill className="valid"/>SETTINGS</Link></li></ul>
+                  
+                </li>
+              </ul>
+            </div>
+      
+            
+        );
+      }
     
 
 export default Home
