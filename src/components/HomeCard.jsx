a541c98890e1b279e45f45d55adef629ae2871ba
fayahmac <bed-com-03-19@unima.ac.mw>
2024-04-23 17:41:37 +0200

    added the icons in the cards components

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import './HomeCard.css'; 
+import {  BsFillArchiveFill, BsFillBellFill, BsFillGearFill, BsFillPeopleFill, BsListCheck } from 'react-icons/bs';
 
 function HomeCard() {
   return (
     <div className="cardview">
       <div className="row">
-        <div className="card blue"></div>
-        <div className="card green"></div>
-        <div className="card orange"></div>
+        <div className="card blue">
+        <div>
+        <h3><BsFillArchiveFill className='icon'/> Program Rules Management</h3> 
+        </div>
+        </div>
+        <div className="card green">
+        <div>
+        <h3><BsListCheck className='icon'/> Rule Validator</h3> 
+        </div>
+        </div>
+        <div className="card orange">
+        <div>
+        <h3><BsFillBellFill className='icon'/> Notifications</h3> 
+        </div>
+        </div>
       </div>
       <div className="row">
-        <div className="card red big"></div>
-        <div className="card gray"></div>
+        <div className="card red big">
+        <div>
+        <h3><BsFillGearFill className='icon'/> Configuration and Troubleshooting Engine</h3> 
+        </div>
+        </div>
+        <div className="card gray">
+        <div>
+        <h3><BsFillPeopleFill className='icon'/> User Manager</h3> 
+        </div>
+        </div>
       </div>
     </div>
   );
