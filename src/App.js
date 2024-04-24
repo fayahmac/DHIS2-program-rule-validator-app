1dbf507c8a51585ad9853cc993185c7f380c77b2
fayahmac <bed-com-03-19@unima.ac.mw>
2024-04-24 15:37:05 +0200

    added the responsiveness ability to the cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
