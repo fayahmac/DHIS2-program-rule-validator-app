a1589e47e46c474df1a8e90f9db284acffe85098
VAN FAYAH <BED/COM/03/19@COMLAB.COM>
2024-04-24 16:45:35 +0200

    created a functionality on homecard which getting program rules data from the dhis2 repository and fetchg it on the card display

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
-import Sidebar from './components/Sidebar'
 
 function App() {
   return (
