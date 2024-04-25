df44be6e35c9b3e90cbe580eff7538d06240beb8
Joseph Jika <bed-com-01-19@unima.ac.mw>
2024-04-25 09:06:42 +0200

    added functions to allow admin to edit and update users

--- src/UserManager.js
+++ src/UserManager.js
@@ -4,63 +4,64 @@ const UserManager = () => {
   // Defining state variables
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState({ name: '', email: '' });
-  //const [editingIndex, setEditingIndex] = useState(-1);
+  const [editingIndex, setEditingIndex] = useState(-1);
 
   // Function to add a new user
   const addUser = () => {
-      // Adding logic to add a new use
+    if (editingIndex !== -1) {
+      const updatedUsers = [...users];
+      updatedUsers[editingIndex] = newUser;
+      setUsers(updatedUsers);
+      setNewUser({ name: '', email: '' });
+      setEditingIndex(-1);
+    } else {
       setUsers([...users, newUser]);
       setNewUser({ name: '', email: '' });
-    };
-  
-    const deleteUser = (index) => {
-      //delete users
-      const updatedUsers = [...users];
-      updatedUsers.splice(index, 1);
-      setUsers(updatedUsers);
-    };
-  
+    }
+  };
+
+      // func to edit users
+  const editUser = (index) => {
+    setNewUser(users[index]);
+    setEditingIndex(index);
+  };
+
+      //func to delete users
+  const deleteUser = (index) => {
+    const updatedUsers = [...users];
+    updatedUsers.splice(index, 1);
+    setUsers(updatedUsers);
+  };
+
+        //save changes
     const handleChange = (e) => {
       const { name, value } = e.target;
       setNewUser({ ...newUser, [name]: value });
     };
   
-    return (
+ 
+  return (
+    <div>
+      <h2>Manage users</h2>
       <div>
-        <h2>User Manager</h2>
-        <div>
-          <input type="text" name="name" value={newUser.name} onChange={handleChange} placeholder="Name" />
-          <input type="email" name="email" value={newUser.email} onChange={handleChange} placeholder="Email" />
-          <button onClick={addUser}>Add User</button>
-        </div>
-        <ul>
-          {users.map((user, index) => (
-            <li key={index}>
-              {user.name} - {user.email} <button onClick={() => deleteUser(index)}>Delete</button>
-            </li>
-          ))}
-        </ul>
+        <input type="text" name="name" value={newUser.name} onChange={handleChange} placeholder="Name" />
+        <input type="email" name="email" value={newUser.email} onChange={handleChange} placeholder="Email" />
+        <button onClick={addUser}>{editingIndex !== -1 ? 'Update User' : 'Add User'}</button>
       </div>
-    );
-  };
-  
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            {user.name} - {user.email} 
+            <button onClick={() => editUser(index)}>Edit this user</button>
+            <button onClick={() => deleteUser(index)}>Delete this user</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
  
 
-
-
-
-
-//   return (
-//     <div>
-//       <h2>User Manager</h2>
-//       <button onClick={addUser}>Add User</button>
-//       <ul>
-//         {users.map((user, index) => (
-//           <li key={index}>{user.name}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
 
 export default UserManager;
