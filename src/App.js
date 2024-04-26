78dff3d3d4bdb651f5a117ba9bea0a6366314bb1
bed-phy-52-19 <bed-phy-52-19@unima.ac.mw>
2024-04-26 13:07:15 +0200

    marge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { DataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
-
+import './App.css'
 // import classes from './App.module.css'
 import Home from './home/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homeee from './components/Homeee'
+import Header from './components/Header'
+
     const MyApp = () => {
       return (
+        
+      
        <BrowserRouter>
+       
+<div className='container'>
+
        <Routes>
         <Route>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         </Route>
               <Route path="/program-rules">
                 {/* Component for Program Rules */}
@@ -33,8 +41,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
         
     
         </Routes>
-        
+        <div className='container-grid'>
+        <Header/>
+        <Homeee/>
+        </div>
+   
+        </div>
        </BrowserRouter>
+     
       );
     }
     
