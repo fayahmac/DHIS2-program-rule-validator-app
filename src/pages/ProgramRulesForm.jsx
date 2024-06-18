b5c1ea783dd49f89024f4a0b90cbf79e6335c8f7
fayahmac <bed-com-03-19@unima.ac.mw>
2024-06-18 18:21:35 +0200

    improved the form format and rayout

--- src/pages/ProgramRulesForm.jsx
+++ src/pages/ProgramRulesForm.jsx
@@ -24,7 +24,8 @@ const ProgramRulesForm = () => {
         actionData: '',
         dataElementId: ''
     });
-
+ 
+    const [selectedFunction, setSelectedFunction] = useState('');
     const [condition, setCondition] = useState('');
     const [isSyntaxCorrect, setIsSyntaxCorrect] = useState(null);
     const [programs, setPrograms] = useState([]);
@@ -103,6 +104,7 @@ const ProgramRulesForm = () => {
             setCondition(value);
             setProgramRule({ ...programRule, condition: value });
         } else if (name === 'function') {
+            setSelectedFunction(value);
             setCondition(prevCondition => prevCondition + value);
             setProgramRule({ ...programRule, condition: condition + value });
         } else if (name === 'variable') {
@@ -113,8 +115,7 @@ const ProgramRulesForm = () => {
             setProgramRule({ ...programRule, [name]: value });
         }
     };
-
-    const fetchNewId = async () => {
+       const fetchNewId = async () => {
         try {
             const response = await fetch('http://localhost:8080/api/system/id', {
                 method: 'GET',
@@ -214,6 +215,33 @@ const ProgramRulesForm = () => {
     const handleValidation = () => {
         setIsDialogOpen(false);
     };
+    const operatorMapping = {
+        '+': '+',
+        '-': '-',
+        '*': '*',
+        '/': '/',
+        '%': '%',
+        '<': '<',
+        '>=': '>=',
+        '<=': '<=',
+        '==': '==',
+        '!=': '!=',
+        'NOT': '!',
+        'AND': '&&',
+        'OR': '||'
+    };
+    const getSyntaxMessage = () => {
+        switch (isSyntaxCorrect) {
+            case 1:
+                return "Syntax is incorrect!";
+            case 2:
+                return "Syntax is correct!";
+            case 3:
+                return "Expression is empty";
+            default:
+                return "";
+        }
+    };
 
     return (
         <form onSubmit={handleSubmit}>
@@ -240,16 +268,89 @@ const ProgramRulesForm = () => {
                     <label>Description</label>
                     <input className="form-input" type="text" name="description" value={programRule.description} onChange={handleChange} placeholder="Description" />
                 </div>
-                <div className="form-group">
-                    <label>Condition</label>
-                    <input className="form-input" type="text" name="condition" value={programRule.condition} onChange={handleChange} placeholder="Condition" />
+                <h4 className='section1'><span className="circle">2</span> Enter program rule expression</h4>
+                <label>Condition</label>
+                <div className="form-g">
+                    <textarea className="form-condition"
+                        value={programRule.condition}
+                        onChange={handleChange}
+                        placeholder="Enter condition here"
+                        name="condition"
+                        disabled={!programRule.program}
+                    />
+                    <div className='form-option'>
+                        <select className="form-input" value={selectedFunction} name="function" onChange={handleChange} disabled={!programRule.program}>
+                            <option value="">Built-in Function</option>
+                            <option value="V{current_date}">V {'{current_date}'}</option>
+                            <option value="V{event_date}">V {'{event_date}'}</option>
+                            <option value="V{due_date}">V {'{due_date}'}</option>
+                            <option value="V{event_count}">V {'{event_count}'}</option>
+                            <option value="V{enrollment_date}">V {'{enrollment_date}'}</option>
+                            <option value="V{incident_date}">V {'{incident_date}'}</option>
+                            <option value="V{enrollment_id}">V {'{enrollment_id}'}</option>
+                            <option value="V{environment}">V {'{environment}'}</option>  
+                            <option value="V{event_id}">V {'{event_id}'}</option>
+                            <option value="V{orgunit_code}">V {'{orgunit_code}'}</option>
+                            <option value="V{program_stage_name}">V {'{program_stage_name}'}</option>
+                            <option value="V{program_stage_id}">V {'{program_stage_id}'}</option>
+                        </select>
+                        <select className="form-input" name="variable" onChange={handleChange} placeholder="Variable" disabled={!programRule.program}>
+                            <option value="">Variables</option>
+                            {variables.map(variable => (
+                                <option key={variable.id} value={variable.displayName}>{variable.displayName}</option>
+                            ))}
+                        </select>
+                        <select className="form-input" value={selectedFunction} name="function" onChange={handleChange} disabled={!programRule.program}>
+                            <option value="">Function</option>
+                            <option value="d2:ceil (<number>)">d2:ceil {'(<number>)'}</option>
+                            <option value="d2:floor (<number>)">d2:floor {'(<number>)'}</option>
+                            <option value="d2:round (<number>)">d2:round {'(<number>)'}</option>
+                            <option value="d2:modulus (<number>,<number>)">d2:modulus {'(<number>,<number>)'}</option>
+                            <option value="d2:zing (<number>)">d2:zing {'(<number>)'}</option>
+                            <option value="d2:oizp (<number>)">d2:oizp {'(<number>)'}</option>
+                            <option value="d2:concatenate (<object>,<object>)">d2:concatenate {'(<object>,<object>)'}</option>
+                            <option value="d2:daysBetween (<date>,<date>)">d2:daysBetween {'(<date>,<date>)'}</option>  
+                            <option value="d2:weeksBetween (<date>,<date>)">d2:weeksBetween {'(<date>,<date>)'}</option>
+                            <option value="d2:monthsBetween (<date>,<date>)">d2:monthsBetween {'(<date>,<date>)'}</option>
+                            <option value="d2:yearsBetween (<date>,<date>)">d2:yearsBetween {'(<date>,<date>)'}</option>
+                            <option value="d2:hasValue (<sourcefield>)">d2:hasValue {'(<sourcefield>)'}</option>
+                            <option value="d2:validatePattern (<text>,<regex>)">d2:validatePattern {'(<text>,<regex>)'}</option>
+                            <option value="d2:addDays (<date>,<number>)">d2:addDays {'(<date>,<number>)'}</option>
+                            <option value="d2:countIfValue (<sourcefield>, <value>)">d2:countIfValue {'(<sourcefield>, <value>)'}</option>
+                            <option value="d2:countIfZeroPos (<sourcefield>)">d2:countIfZeroPos {'(<sourcefield>)'}</option>
+                            <option value="d2:hasValue (<sourcefield>)">d2:hasValue {'(<sourcefield>)'}</option>
+                            <option value="d2:zpvc (<object>,<object>)">d2:zpvc {'(<object>,<object>)'}</option>
+                            <option value="d2:validatePatterns (<text>,<regex)">d2:validatePatterns {'(<text>,<regex)'}</option>
+                            <option value="d2:left (<text>,<number>)">d2:left {'(<text>,<number>)'}</option>
+                            <option value="d2:right (<text>,<number>)">d2:right {'(<text>,<number>)'}</option>
+                            <option value="d2:substring (<text>,<number>,<number>)">d2:substring {'(<text>,<number>,<number>)'}</option>  
+                            <option value="d2:split (<text>,<text>,<number>)">d2:split {'(<text>,<text>,<number>)'}</option>
+                            <option value="d2:length (<text>)">d2:length {'(<text>)'}</option>
+                            <option value="d2:inOrgUnitGroup( <orgunit_group_code> )">d2:inOrgUnitGroup{'( <orgunit_group_code> )'}</option>
+                            <option value="d2:hasUserRole( <user_role> )">d2:hasUserRole{'( <user_role> )'}</option>
+                            <option value="d2:zScoreWFA( <ageInMonth>, <weight>, <gender> )">d2:zScoreWFA{'( <ageInMonth>, <weight>, <gender> )'}</option>  
+                            <option value="d2:zScoreHFA( <ageInMonth>, <height>, <gender> )">d2:zScoreHFA{'( <ageInMonth>, <height>, <gender> )'}</option>
+                            <option value="d2:zScoreWFH( <height>, <weight>, <gender> )">d2:zScoreWFH{'( <height>, <weight>, <gender> )'}</option>
+                            <option value="d2:extractDataMatrixValue( <key>, <value>)">d2:extractDataMatrixValue{'( <key>, <value>)'}</option>
+                        </select>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label>Program Rule Variable</label>
-                    <DropdownButton items={variables} name="variable" handleChange={handleChange} />
+                <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap', marginBottom: '10px' }}  >
+                    {Object.keys(operatorMapping).map((displayLabel) => (
+                        <span
+                            key={displayLabel}
+                            onClick={() => handleOperatorClick(operatorMapping[displayLabel])}
+                            style={{ padding: '5px 10px', cursor: 'pointer', fontSize: '21px', borderRadius: '4px' }} 
+                            disabled={!programRule.program}
+                        >
+                            {displayLabel}
+                        </span>
+                    ))}
+                    <p>{getSyntaxMessage()}</p>
                 </div>
+                <h4 className='section1'><span className="circle">3</span> Enter program rule action details</h4>
                 <div className="form-group">
-                    <label>Function</label>
+                <label>Program Rule Action Type</label>
                     <DropdownButton
                         items={[
                             { id: '(', displayName: '(' },
